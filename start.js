e00c332f135513668b19e09799c0792a0b3b4605
Guilherme Toni <guilhermedelly8@gmail.com>
2024-10-22 16:45:09 -0300

    feat: add cors

--- start.js
+++ start.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./src/routes/index.js');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

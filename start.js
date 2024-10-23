c0c2e1c700be8ab009be3f777c53ddc81a2116df
Guilherme Toni <guilhermedelly8@gmail.com>
2024-10-23 14:18:41 -0300

    fix: remove base route on /

--- start.js
+++ start.js
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
-  res.json({ message: 'Success' });
-});
-
 app.use('/api/v1', routes);
 
 app.listen(port, () => {
